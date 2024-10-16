2411dffb32651a685ca39ac567825e6b68f7e152
snehakore54 <151037050+snehakore54@users.noreply.github.com>
2024-10-16 14:18:59 +0530

    navbar section to be displayed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Navbar from './components/Navbar';
 import Features from './components/Features';
 import WhyUs from './components/WhyUs'
 import Tokenomics from './components/Tokenomics'
@@ -14,7 +13,43 @@ function App() {
     <div className=''>
       <div className='app-container'>
       
-      <Navbar />
+      
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div className="container">
+                <div className='img'>
+                <img src={`${process.env.PUBLIC_URL}/Frame43094logo.png`} alt="Logo" />
+                </div>
+                <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-toggle="collapse"
+                    data-target="#navbarNavAltMarkup"
+                    aria-controls="navbarNavAltMarkup"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                >
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div className="navItems navbar-nav">
+                        <a className="nav-link active" href="#sectionFeatures">
+                            Features<span className="sr-only">(current)</span>
+                        </a>
+                        <a className="nav-link" href="#sectionWhyUs">Why Us</a>
+                        <a className="nav-link" href="#sectionTokenomics">Tokenomics</a>
+                        <a className="nav-link" href="#sectionRoadmap">Roadmap</a>
+                    </div>
+                    <div className="d-none d-lg-block ml-5">
+                        <button className="logBtn mr-3">Log in</button>
+                        <button className="whiteBtn">Whitepaper</button>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+
+
+
       <video autoPlay muted loop className="background-video">
         <source src={process.env.PUBLIC_URL + '/-c85c-4755-a1c0-4646ee276bd5.mp4'} type="video/mp4" />
         Your browser does not support the video tag.
